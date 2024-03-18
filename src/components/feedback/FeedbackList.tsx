e86d83941dceb6905fa570c15362e4d81818105b
cyclops <mcknight.jeremy@gmail.com>
2024-03-17 21:05:48 -0700

    remove error message

--- src/components/feedback/FeedbackList.tsx
+++ src/components/feedback/FeedbackList.tsx
@@ -1,6 +1,5 @@
 import FeedbackItem from "./FeedbackItem";
 import Spinner from "../Spinner";
-import ErrorMessage from "../ErrorMessage";
 import { useFeedbackStore } from "../../stores/feedback.store";
 
 const FeedbackList = () => {
@@ -18,7 +17,6 @@ const FeedbackList = () => {
   return (
     <ol className="feedback-list">
       {loading && <Spinner />}
-      {error && <ErrorMessage message={error} />}
 
       {feedbackList.map((feedbackItem) => (
         <FeedbackItem key={feedbackItem.id} feedback={feedbackItem} />
